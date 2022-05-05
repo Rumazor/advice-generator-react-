d19de6feb3bf86c74cabd39f0941f69a53592441
Rumazor <th3rum2084@gmail.com>
2022-05-05 01:58:39 -0500

    master

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState,useEffect } from 'react'
+
+import images from './images/patern.svg'
+import image from './images/icon-dice.svg'
+
+
+// Component
+const App = () => {
+
+const url = 'https://api.adviceslip.com/advice';
+const [ tip,setAdvice ] = useState([]);
+
+const getAdvice = async ()=>{
+    const response = await fetch(url);
+    const tip = await response.json()
+    setAdvice([tip]) 
+}
+
+useEffect(()=>{
+getAdvice()
+},[])
+
+const newAdvice = ()=>{
+    getAdvice()
+}
+
+    return (
+    <div className='advice-box'>
+        <article className='box'>
+            {tip.map((item)=>{
+                const {slip} = item;
+                const {id,advice} = slip
+
+            return <div key={id}>
+                <h4 className='id-advice'>ADVICE #{id}</h4>
+                <p key={id}>{advice}</p>
+            </div>
+            })}
+            <img className='box-img' src={images} alt="" />
+            <button className='btn' onClick={newAdvice}>
+                <img className='dice-img' src={image} alt="" />
+            </button>
+        </article>
+    </div>
+    )
+}
+
+export default App
